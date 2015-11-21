5824306b76c43265833d547e17c5b840fe8d76e8
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-11-21 15:12:21 +0100

    Fixed response navbar

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {Navbar, NavbarBrand, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import Icon from './common/Icon';
 import _ from 'lodash';
@@ -18,58 +18,65 @@ class Menu extends React.Component {
 
     return (
       <Navbar inverse>
-        <NavbarBrand><Link to="/">Game Site</Link></NavbarBrand>
-        <Nav>
-          {
-            links.map(link => {
-              return (
-                 <LinkContainer key={link.href} to={link.href}>
-                   <NavItem>{link.text}</NavItem>
-                 </LinkContainer>
-              );
-            })
-          }
-        </Nav>
-        {
-          () => {
-            if (loggedIn) {
-              const myGames = _.pick(this.props.games, (game, gameId) => {
-                return _.contains(game.players, id);
-              });
-
-              return (
-                <Nav pullRight>
-                  {
-                    !_.isEmpty(myGames) ?
-                    <NavDropdown title="My games" id="nav-games-dropdown">
-                      {
-                        _.map(myGames, (game, gameId) => {
-                          return (
-                            <LinkContainer key={gameId} to={`/game/${gameId}`}>
-                              <MenuItem>{gameId}</MenuItem>
-                            </LinkContainer>
-                          );
-                        })
-                      }
-                    </NavDropdown> : null
-                  }
-                  <NavDropdown title={username} id="nav-profile-dropdown">
-                    <MenuItem onSelect={logOut}><Icon type='sign-out'/> Log out</MenuItem>
-                  </NavDropdown>
-                </Nav>
-              );
-
-            } else {
-              return (
-                <Nav pullRight>
-                  <LinkContainer to="/login">
-                    <NavItem><Icon type='sign-in'/> Log in</NavItem>
-                  </LinkContainer>
-                </Nav>
-              );
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Game Site</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            {
+              links.map(link => {
+                return (
+                   <LinkContainer key={link.href} to={link.href}>
+                     <NavItem>{link.text}</NavItem>
+                   </LinkContainer>
+                );
+              })
             }
-          }()
-        }
+          </Nav>
+          {
+            () => {
+              if (loggedIn) {
+                const myGames = _.pick(this.props.games, (game, gameId) => {
+                  return _.contains(game.players, id);
+                });
+
+                return (
+                  <Nav pullRight>
+                    {
+                      !_.isEmpty(myGames) ?
+                      <NavDropdown title="My games" id="nav-games-dropdown">
+                        {
+                          _.map(myGames, (game, gameId) => {
+                            return (
+                              <LinkContainer key={gameId} to={`/game/${gameId}`}>
+                                <MenuItem>{gameId}</MenuItem>
+                              </LinkContainer>
+                            );
+                          })
+                        }
+                      </NavDropdown> : null
+                    }
+                    <NavDropdown title={username} id="nav-profile-dropdown">
+                      <MenuItem onSelect={logOut}><Icon type='sign-out'/> Log out</MenuItem>
+                    </NavDropdown>
+                  </Nav>
+                );
+
+              } else {
+                return (
+                  <Nav pullRight>
+                    <LinkContainer to="/login">
+                      <NavItem><Icon type='sign-in'/> Log in</NavItem>
+                    </LinkContainer>
+                  </Nav>
+                );
+              }
+            }()
+          }
+        </Navbar.Collapse>
       </Navbar>
     );
   }
