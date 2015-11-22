0638202765f0c8d7b82b5b89c7da9c2a541d7638
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-11-22 17:47:14 +0100

    Reorganized love letter tests

--- test/server/loveLetter/loveLetter.spec.js
+++ test/server/loveLetter/loveLetter.spec.js
@@ -3,31 +3,31 @@ import {cards} from '../../../server/loveLetterCards';
 import {expect} from 'chai';
 
 describe('love letter', () => {
-  it('creates correct initial state for 2 player game', () => {
-    const state = loveLetter.createInitialState(['Bob', 'Jack']);
-    const firstPlayer = state.players[state.order[0]];
+  describe('game initial state', () => {
+    it('deals two cards to first player', () => {
+      const state = loveLetter.createInitialState(['Bob', 'Jack']);
+      const firstPlayer = state.players[state.toAct];
 
-    expect(firstPlayer.hand.length).to.equal(2);
-    expect(state.toAct).to.equal(state.order[0]);
-    expect(state.deck.length).to.equal(13);
-  });
+      expect(firstPlayer.hand.length).to.equal(2);
+    });
 
-  it('creates correct initial state for 3 player game', () => {
-    const state = loveLetter.createInitialState(['Bob', 'Jack', 'Jill']);
-    const firstPlayer = state.players[state.order[0]];
+    it('deck has 13 cards in 2 player game', () => {
+      const state = loveLetter.createInitialState(['Bob', 'Jack']);
 
-    expect(firstPlayer.hand.length).to.equal(2);
-    expect(state.toAct).to.equal(state.order[0]);
-    expect(state.deck.length).to.equal(12);
-  });
+      expect(state.deck.length).to.equal(13);
+    });
 
-  it('creates correct initial state for 4 player game', () => {
-    const state = loveLetter.createInitialState(['Bob', 'Jack', 'Jill', 'Chris']);
-    const firstPlayer = state.players[state.order[0]];
+    it('deck has 12 cards in 3 player game', () => {
+      const state = loveLetter.createInitialState(['Bob', 'Jack', 'Jill']);
 
-    expect(firstPlayer.hand.length).to.equal(2);
-    expect(state.toAct).to.equal(state.order[0]);
-    expect(state.deck.length).to.equal(11);
+      expect(state.deck.length).to.equal(12);
+    });
+
+    it('deck has 11 cards in 4 player game', () => {
+      const state = loveLetter.createInitialState(['Bob', 'Jack', 'Jill', 'Chris']);
+
+      expect(state.deck.length).to.equal(11);
+    });
   });
 
   describe('secret information', () => {
