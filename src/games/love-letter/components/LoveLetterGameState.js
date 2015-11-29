f9af2521a2b6af62ca73f9028bdb2881cb5bcba0
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-11-29 14:28:22 +0100

    Show players in order

--- src/games/love-letter/components/LoveLetterGameState.js
+++ src/games/love-letter/components/LoveLetterGameState.js
@@ -17,23 +17,35 @@ export default class LoveLetterGameState extends React.Component {
       return <Alert bsStyle='warning'>Game in progress. Spectating coming soon.</Alert>;
     }
 
-    const playerStates = _.map(game.state.players, (playerState, id) => {
-      return (
-        <Panel key={id}>
-          <Icon type="heart" /> {playerState.score}
-          {' '}
-          <ProtectedIcon protect={playerState.protected} /> <Avatar players={players} id={id} size="S" />
-          {' '}
-          {players[id].name}
-          {game.state.toAct === id ? <span> <WaitingIcon /></span> : null}
-          <span> {_.isEmpty(playerState.hand) ? 'DEAD' : 'ALIVE'}</span>
-          {' '}
-          <span>
-            <label>Discards:</label> {playerState.discards.join(', ')}
-          </span>
-        </Panel>
-      );
-    });
+    const playerStates = _(game.state.players)
+      .map((playerState, id) => {
+        return {
+          id,
+          ...playerState
+        };
+      })
+      .sortBy(playerState => {
+        return _.indexOf(game.state.order, playerState.id);
+      })
+      .map(playerState => {
+        const id = playerState.id;
+        return (
+          <Panel key={id}>
+            <Icon type="heart" /> {playerState.score}
+            {' '}
+            <ProtectedIcon protect={playerState.protected} /> <Avatar players={players} id={id} size="S" />
+            {' '}
+            {players[id].name}
+            {game.state.toAct === id ? <span> <WaitingIcon /></span> : null}
+            <span> {_.isEmpty(playerState.hand) ? 'DEAD' : 'ALIVE'}</span>
+            {' '}
+            <span>
+              <label>Discards:</label> {playerState.discards.join(', ')}
+            </span>
+          </Panel>
+        );
+      })
+      .value();
 
     return (
       <div>
