e70b0036cb9e9c0a15b82eaa8292e91d9de58f3d
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-12-05 14:24:17 +0100

    Added container class to create game form

--- src/components/lobby/CreateGame.js
+++ src/components/lobby/CreateGame.js
@@ -21,27 +21,32 @@ export default class CreateGame extends React.Component {
   render() {
     const {loggedIn} = this.props.login;
 
-    return !loggedIn ?
-    <Alert bsStyle='warning'>
-      You must be logged in to create a game.
-    </Alert>
-    :
-    <Panel>
-      <form onSubmit={this.createGame}>
-        <Alert bsStyle='warning'>
-          Game options are coming soon.
-        </Alert>
-
-        <Button
-          type='submit'
-          bsStyle='primary'>
-          Create game
-        </Button>
-        {' '}
-        <LinkContainer to='/lobby'>
-          <Button>Cancel</Button>
-        </LinkContainer>
-      </form>
-    </Panel>;
+    return (
+      <div className="container">
+        {
+          !loggedIn ?
+          <Alert bsStyle='warning'>
+            You must be logged in to create a game.
+          </Alert>
+          :
+          <Panel>
+            <form onSubmit={this.createGame}>
+              <Alert bsStyle='warning'>
+                Game options are coming soon.
+              </Alert>
+              <Button
+                type='submit'
+                bsStyle='primary'>
+                Create game
+              </Button>
+              {' '}
+              <LinkContainer to='/lobby'>
+                <Button>Cancel</Button>
+              </LinkContainer>
+            </form>
+          </Panel>
+        }
+      </div>
+    );
   }
 }
