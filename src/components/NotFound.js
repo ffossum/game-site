d192f3469a8da67d86874939855d3f37d6078a62
Fredrik Fossum <fredrik.fossum@gmail.com>
2015-12-12 12:51:28 +0100

    Styled 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Panel} from './common';
+import DocumentTitle from 'react-document-title';
 
-export default props => (
-  <div>
-    <h1>404</h1>
-    <p>This isn't the page you're looking for.</p>
-  </div>
-);
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <DocumentTitle title="Not found">
+        <div className="container">
+          <Panel>
+            <h1>404</h1>
+            <p>This isn't the page you're looking for.</p>
+          </Panel>
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
