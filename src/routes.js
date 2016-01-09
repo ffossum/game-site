6c1fc68c96c1d7f28b6a11b8d5f8c83ed0eee8bf
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-01-09 23:22:20 +0100

    Added webpack code splitting

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
-import FrontPage from './components/FrontPage';
-import Lobby from './containers/LobbyContainer';
-import Create from './containers/CreateGameContainer';
-import Game from './containers/GameContainer';
-import About from './components/About';
 import NotFound from './components/NotFound';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={FrontPage} />
-    <Route path="lobby" component={Lobby} />
-    <Route path="create" component={Create} />
-    <Route path="game/:id" component={Game} />
-    <Route path="about" component={About} />
+    <IndexRoute getComponent={getFrontPage} />
+    <Route path="lobby" getComponent={getLobby} />
+    <Route path="create" getComponent={getCreateGame} />
+    <Route path="game/:id" getComponent={getGame} />
+    <Route path="about" getComponent={getAbout} />
     <Route path="*" component={NotFound} />
   </Route>
 );
+
+function getFrontPage(location, cb) {
+  require.ensure([], require => {
+    cb(null, require('./components/FrontPage'));
+  });
+}
+
+function getLobby(location, cb) {
+  require.ensure([], require => {
+    cb(null, require('./containers/LobbyContainer'));
+  });
+}
+
+function getCreateGame(location, cb) {
+  require.ensure([], require => {
+    cb(null, require('./containers/CreateGameContainer'));
+  });
+}
+
+function getGame(location, cb) {
+  require.ensure([], require => {
+    cb(null, require('./containers/GameContainer'));
+  });
+}
+
+function getAbout(location, cb) {
+  require.ensure([], require => {
+    cb(null, require('./components/About'));
+  });
+}
